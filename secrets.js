deec172a56f5f15435df856da84ce40e7fe85dd4
Markus Englund <markus.s.englund@gmail.com>
2017-01-29 16:04:29 +0100

    .gitignore

--- secrets.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    yelpToken: "HFo75c9KB2HkcPiTsClHBPiT1KyB6QE6uyRGkBK8cjnvZXXgQ7rAPznvMEU_khbgx1NiGxFywyRO6pjL-N8lVrBbkimw_eSzALDdyKR_pb8nWU9toSnY_7Vt-f6JWHYx",
-    twitterKey: 'qTaYtSjSXGDtrdn7Utgw8PnNx',
-    twitterSecret: '31v14Do5V7w4UfVQd1CpX2Y8glV9hoq4vlwNizcXmTAFw2o921',
-    mongooseConnect: "mongodb://markus:markus@ds163738.mlab.com:63738/nightlife-app"
-}
